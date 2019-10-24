35436a2a4aa08461b6f6caeed7c87f8978414c02
Michael Borglin <michael@team.casa>
2019-10-24 02:59:00 -0600

    Started setting up intro templates and environment variable test

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     </header>
 
     <main>
-      <LoadingBar percent="50" />
+      <LoadingBar :percent="loading" />
     </main>
 
     <footer>
@@ -16,10 +16,35 @@
 
 <script>
   import LoadingBar from '@/components/LoadingBar';
+  import API from '@/helpers/api';
 
   export default {
     components: {
       LoadingBar,
+    },
+
+    data() {
+      return {
+        loading: 0,
+      }
+    },
+
+    async created() {
+      let apiUrl = process.env.DEVICE_HOST;
+
+      if(window.location.href.includes('.onion')) {
+        apiUrl = process.env.CASA_NODE_HIDDEN_SERVICE;
+      }
+
+      alert('generated API URL - ' + apiUrl);
+
+      const loading = await API.get(this.$axios, `${apiUrl}:3000/v1/telemetry/boot`);
+
+      // If there is a network failure, an exception will be thrown and loading will return false
+      if(loading) {
+        console.log("yay it loaded", loading);
+        this.loading = 100;
+      }
     },
   }
 </script>
---
+++ pages/intro/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      Intro
+    </header>
+
+    <main>
+      <LoadingBar percent="50" />
+    </main>
+
+    <footer>
+      Footer Buttons
+    </footer>
+  </div>
+</template>
+
+<script>
+  import LoadingBar from '@/components/LoadingBar';
+
+  export default {
+    components: {
+      LoadingBar,
+    },
+  }
+</script>
