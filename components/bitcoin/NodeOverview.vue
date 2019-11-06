4864fd48876766de9842174a2a3b303a054a8c2b
Michael Borglin <michael@team.casa>
2019-11-06 03:18:57 -0700

    Finished bitcoin node overview styles

--- components/bitcoin/NodeOverview.vue
+++ components/bitcoin/NodeOverview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div class="card node-overview">
     <div class="flex space-between">
       <h3>Node Overview</h3>
       <a class="button">Manage Bitcoin Node</a>
@@ -7,7 +7,7 @@
 
     <div class="columns space-between">
       <div class="column narrow">
-        <h1>
+        <h1 class="status active">
           Online
         </h1>
 
@@ -21,32 +21,76 @@
       </div>
 
       <div class="column narrow">
-        <h1>
-          Online
+        <h1 class="numeric status syncing">
+          98%
         </h1>
 
-        <h6 class="numeric">
-          172.91.133.204
+        <h6>
+          <span class="numeric">584,768</span>
+          <span class="spacer">of</span>
+          <span class="numeric">596,703</span>
         </h6>
 
         <div class="label">
-          Connection Status
+          Block Sync Status
         </div>
       </div>
 
       <div class="column narrow">
         <h1>
-          Online
+          <span class="numeric">8</span>
+          Outbound
         </h1>
 
-        <h6 class="numeric">
-          172.91.133.204
+        <h6>
+          <span class="numeric">10</span>
+          Inbound
         </h6>
 
         <div class="label">
-          Connection Status
+          Connected Peers
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style lang="scss">
+  @import "~/assets/css/variables.scss";
+
+  .node-overview {
+    h1 {
+      font-weight: bold;
+
+      &.numeric {
+        font-weight: normal;
+      }
+    }
+
+    h6 {
+      color: $gray;
+      font-weight: normal;
+    }
+
+    .spacer {
+      margin: 0 0.4em;
+    }
+
+    .button {
+      margin-top: -0.25em;
+      margin-right: -0.25em;
+    }
+
+    .columns {
+      margin-top: 2em;
+    }
+
+    .column {
+      min-width: 250px;
+    }
+
+    .label {
+      margin-top: 2em;
+    }
+  }
+</style>
