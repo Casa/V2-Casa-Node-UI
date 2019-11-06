4caa26ab32dbf040df9d781744e69c9f48b87082
Michael Borglin <michael@team.casa>
2019-11-05 23:51:34 -0700

    Bitcoin page template structure

--- components/bitcoin/cards/NodeOverview.vue
+++ components/bitcoin/cards/NodeOverview.vue
@@ -4,5 +4,49 @@
       <h3>Node Overview</h3>
       <a class="button">Manage Bitcoin Node</a>
     </div>
+
+    <div class="columns space-between">
+      <div class="column narrow">
+        <h1>
+          Online
+        </h1>
+
+        <h6 class="numeric">
+          172.91.133.204
+        </h6>
+
+        <div class="label">
+          Connection Status
+        </div>
+      </div>
+
+      <div class="column narrow">
+        <h1>
+          Online
+        </h1>
+
+        <h6 class="numeric">
+          172.91.133.204
+        </h6>
+
+        <div class="label">
+          Connection Status
+        </div>
+      </div>
+
+      <div class="column narrow">
+        <h1>
+          Online
+        </h1>
+
+        <h6 class="numeric">
+          172.91.133.204
+        </h6>
+
+        <div class="label">
+          Connection Status
+        </div>
+      </div>
+    </div>
   </div>
 </template>
