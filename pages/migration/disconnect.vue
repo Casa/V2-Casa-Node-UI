4ffef2b8fffb9b197339ccb8ae87ec8d2ecc0efe
Michael Borglin <michael@team.casa>
2019-11-18 18:08:28 -0800

    Fix syntax

--- pages/migration/disconnect.vue
+++ pages/migration/disconnect.vue
@@ -3,9 +3,10 @@
     <main>
       <img src="~/assets/icons/adapter.svg">
 
-      <h2 class="headline">Almost there. Please unplug the hard drive cable from the back of Casa Node 2.</h2>
+      <h2 class="headline">
+        Almost there. Please unplug the hard drive cable from the back of Casa Node 2.
+      </h2>
     </main>
-
     <footer>
       <nuxt-link to="/home" class="button is-primary">
         Start using Casa Node 2
