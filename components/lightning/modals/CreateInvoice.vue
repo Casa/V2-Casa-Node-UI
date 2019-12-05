4361c44d8ae34a78cff30e062b6eb039d571db42
Michael Borglin <michael@team.casa>
2019-12-05 16:15:22 -0700

    Started fleshing out create invoice modal

--- components/lightning/modals/CreateInvoice.vue
+++ components/lightning/modals/CreateInvoice.vue
@@ -14,7 +14,30 @@
       </div>
       <hr>
 
-      <InputField label="Payment Code" />
+      <div class="columns">
+        <div class="column is-full">
+          <input v-model="amountDisplayed" class="primary-input numeric" @input="updateAmount" @click="updateAmountDisplayed">
+        </div>
+      </div>
+
+      <div class="columns">
+        <div class="column centered centered-vertically">
+          <template v-if="inputMode === 'usd'">
+            <span class="numeric">{{ amountSats | localized }}</span> sats
+          </template>
+
+          <template v-else-if="inputMode === 'sats'">
+            <span class="numeric">${{ amountUsd | localized }}</span>
+          </template>
+        </div>
+
+        <div class="column right">
+          <a v-if="inputMode === 'usd'" class="button is-small" @click="setInput('sats')">Switch to Sats</a>
+          <a v-else-if="inputMode === 'sats'" class="button is-small" @click="setInput('usd')">Switch to USD</a>
+        </div>
+      </div>
+
+      <InputField label="Memo (Optional)" />
 
       <hr>
 
@@ -37,7 +60,10 @@
   export default {
     data() {
       return {
+        inputMode: 'usd',
         paymentCode: '',
+        amountDisplayed: '$0',
+        amountSats: 0,
       }
     },
 
@@ -53,6 +79,28 @@
   @import "~/assets/css/variables.scss";
 
   .create-invoice-modal {
-    //
+    .modal-content {
+      min-width: 50%;
+    }
+
+    .unit-switch {
+      top: 2em;
+      right: 2.1em;
+    }
+
+    .primary-input {
+      background: none;
+      border: none;
+      outline: none;
+      color: $white;
+      font-size: 60px;
+      text-align: center;
+      margin: 0 auto;
+      display: block;
+    }
+
+    .input-wrap {
+      width: 100%;
+    }
   }
 </style>
