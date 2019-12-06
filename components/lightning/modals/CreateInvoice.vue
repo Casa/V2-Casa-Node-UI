76566b0f721cb34289fb95285c6abd987ea3a2d2
Michael Borglin <michael@team.casa>
2019-12-06 00:47:49 -0700

    Finished input handling code when creating invoices

--- components/lightning/modals/CreateInvoice.vue
+++ components/lightning/modals/CreateInvoice.vue
@@ -21,7 +21,7 @@
       </div>
 
       <div class="columns">
-        <div class="column centered centered-vertically">
+        <div class="column is-offset-one-third centered centered-vertically">
           <template v-if="inputMode === 'usd'">
             <span class="numeric">{{ amountSats | localized }}</span> sats
           </template>
@@ -53,7 +53,7 @@
 </template>
 
 <script>
-//  import {satsToBtc} from '@/helpers/units';
+  import {satsToBtc, btcToSats, toPrecision} from '@/helpers/units';
 //  import API from '@/helpers/api';
 //  import Events from '~/helpers/events';
 
@@ -64,10 +64,65 @@
         paymentCode: '',
         amountDisplayed: '$0',
         amountSats: 0,
+        amountBtc: 0,
       }
     },
 
     methods: {
+      // Automatically clear the input field unless the user has already entered a value
+      updateAmountDisplayed() {
+        if(this.inputMode === 'usd') {
+          if(this.amountUsd) {
+            this.amountDisplayed = '$' + this.amountUsd;
+          } else {
+            this.amountDisplayed = '$';
+          }
+        } else if(this.inputMode === 'sats') {
+          if(this.amountSats) {
+            this.amountDisplayed = this.amountSats;
+         } else {
+            this.amountDisplayed = '';
+          }
+        }
+      },
+
+      // Update the amount displayed and the amount saved in local memory
+      updateAmount(event) {
+        if(this.inputMode === 'usd') {
+          let value = event.target.value.replace(/[^0-9.]/g, '');
+
+          // Force the dollar amount to only have two decimals when it gets too long
+          if(value.match(/\.[0-9]{3,}/)) {
+            // toPrecision() truncates trailing zeroes, using toFixed fixes that
+            value = parseFloat(toPrecision(value, 2)).toFixed(2);
+          }
+
+          this.amountUsd = value;
+          this.amountBtc = value / this.$store.state.bitcoin.price;
+          this.amountSats = Math.round(btcToSats(this.amountBtc));
+        } else if(this.inputMode === 'sats') {
+          let value = parseInt(event.target.value) || 0;
+
+          this.amountSats = value;
+          this.amountBtc = satsToBtc(value);
+          this.amountUsd = parseFloat(this.amountBtc * this.$store.state.bitcoin.price).toFixed(2);
+        }
+
+        this.updateAmountDisplayed();
+      },
+
+      setInput(mode) {
+        this.inputMode = mode;
+
+        if(mode === 'usd') {
+          this.amountDisplayed = '$' + this.amountUsd;
+        } else if(mode === 'sats') {
+          this.amountDisplayed = this.amountSats;
+        } else if(mode === 'btc') {
+          // Todo - Requires btc / sats switch button integrated with the vuex store
+        }
+      },
+
       validate() {
         //
       },
