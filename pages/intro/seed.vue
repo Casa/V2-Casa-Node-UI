1da9554cae62e410d149d458160dfeb98e124ff2
Michael Borglin <michael@team.casa>
2019-12-12 10:46:36 -0800

    Make intro buttons same color

--- pages/intro/seed.vue
+++ pages/intro/seed.vue
@@ -8,11 +8,11 @@
     </main>
 
     <footer>
-      <nuxt-link to="/intro/new-seed" class="button is-light">
+      <nuxt-link to="/intro/new-seed" class="button">
         Generate New Seed Phrase
       </nuxt-link>
 
-      <nuxt-link to="/intro/existing-seed" class="button is-light">
+      <nuxt-link to="/intro/existing-seed" class="button">
         Import Existing Seed Phrase
       </nuxt-link>
     </footer>
