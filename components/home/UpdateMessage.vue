863a83cc2be82b1f8c66ce8d247a53310acbe208
Michael Borglin <michael@team.casa>
2020-01-09 13:40:30 -0800

    Add loading indicator for update button

--- components/home/UpdateMessage.vue
+++ components/home/UpdateMessage.vue
@@ -12,7 +12,7 @@
     </div>
 
     <div class="buttons">
-      <a class="button is-light is-small is-arrow" href="https://github.com/Casa/V2-Casa-Node-UI/blob/develop/CHANGELOG.md">Learn More</a>
+      <a class="button is-light is-small is-arrow" target="_blank" href="https://github.com/Casa/V2-Casa-Node-UI/blob/master/CHANGELOG.md">Learn More</a>
       <a class="button is-primary is-small" @click="$parent.update()">Update Now</a>
     </div>
   </div>
