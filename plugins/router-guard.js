1432eb715d4a50dcf4336f06041d853ae69da6d8
Nick Fogle <nick@team.casa>
2020-01-10 14:36:34 -0500

    Remove logs from plugin

--- plugins/router-guard.js
+++ plugins/router-guard.js
@@ -1,7 +1,5 @@
 export default ({ app }) => {
    app.router.afterEach((to, from) => {
-     console.log('to', to);
-     console.log('from', from);
      if(from.fullPath.includes('intro') && to.fullPath === '/home') {
        localStorage.setItem('welcome', 'true');
      }
